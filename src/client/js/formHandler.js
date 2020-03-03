d732d94c1049a4bb0e8064ea8b9375a60806392f
Abdelwahab1242 <Abdelwahab1242@gmail.com>
2020-03-03 05:41:00 +0200

    First commit

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,48 @@
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const baseURL = "http://localhost:8080/sentiment";
+  const url = document.getElementById("url").value;
+
+  if (isURL(url)) {
+    fetch(baseURL, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ url: url })
+    })
+      .then(res => res.json())
+      .then(res => {
+        document.getElementById(
+          "results"
+        ).innerHTML = `Polarity: ${res.polarity}<br>`;
+        document.getElementById(
+          "results"
+        ).innerHTML = `Subjectivity: ${res.subjectivity}`;
+      });
+  } else {
+    alert("URL is not valid!");
+  }
+
+  // Validate the URL
+  function isURL(str) {
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    ); // fragment locator
+    if (pattern.test(str)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export { handleSubmit };
---
+++ src/server/index.js
@@ -0,0 +1,57 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Requiring dependencies
+var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+var aylien = require("aylien_textapi");
+
+// Setup empty JS object to act as an endpoint
+let projectData = {};
+
+// set aylien API credentias
+var textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
+
+// Start up an instance of app
+const app = express();
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static("dist"));
+
+console.log(__dirname);
+
+app.get("/", function(req, res) {
+  res.sendFile("dist/index.html");
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function() {
+  console.log("Example app listening on port 8080!");
+});
+
+app.post("/sentiment", (req, res) => {
+  const urlUser = req.body.url;
+  textapi.sentiment({ url: urlUser }, (error, response) => {
+    if (error === null) {
+      projectData["polarity"] = response.polarity;
+      projectData["subjectivity"] = response.subjectivity;
+
+      res.send(projectData);
+      console.log(projectData);
+    } else {
+      console.log(error, "Error");
+    }
+  });
+});
